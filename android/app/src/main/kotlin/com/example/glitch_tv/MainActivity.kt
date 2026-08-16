d5e6957bd8c4aacba903443654d2e3ccd0e7de21
ahmed <ahmedmoataz221104@gmail.com>
2026-08-16 20:41:18 +0300

    feat(app-initial): - Add initial packages in pubspec.yaml like flutter_bloc - Setup app icon & name - Build app architecture as clean architecture -  Add app colors

---
+++ android/app/src/main/kotlin/com/example/glitch_tv/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.glitch_tv
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/core/utils/app_colors.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+abstract class AppColors {
+  static const Color primary = Color(0xFF8B2FD9);
+  static const Color primaryLight = Color(0xFFB14EFF);
+  static const Color primaryDark = Color(0xFF5B1A9C);
+  static const Color card = Color(0xFF1C1230);
+  static const Color textPrimary = Color(0xFFFFFFFF);
+  static const Color textSecondary = Color(0xFFB8AECC);
+  static const Color scaffoldBackground = Color(0xFF0F0A1A);
+}
